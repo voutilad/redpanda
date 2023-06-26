bbed12ee70aeae02118d59cb98f578bef0eab22f
Dave Voutila <voutilad@gmail.com>
2023-06-26 10:47:37 -0400

    taking advice from clang-tidy

--- src/ws/main.cc
+++ src/ws/main.cc
@@ -2,11 +2,9 @@
 #include "kafka/client/client.h"
 #include "kafka/client/configuration.h"
 #include "kafka/client/types.h"
-#include "kafka/protocol/fetch.h"
 
 #include <seastar/core/app-template.hh>
 #include <seastar/core/future.hh>
-#include <seastar/core/reactor.hh>
 #include <seastar/core/seastar.hh>
 #include <seastar/core/thread.hh>
 #include <seastar/util/log.hh>
@@ -26,14 +24,12 @@ parse_brokers(std::vector<std::string>& brokers) noexcept {
     std::vector<net::unresolved_address> addrs{};
 
     try {
-        for (auto broker : brokers) {
+        for (const auto& broker : brokers) {
             std::string host{};
-            uint16_t port;
+            uint16_t port = 9092;
 
-            size_t colon = broker.find_first_of(":");
-            if (colon == std::string::npos) {
-                port = 9092;
-            } else {
+            const size_t colon = broker.find_first_of(":");
+            if (colon != std::string::npos) {
                 // xxx catch
                 auto len = broker.size() - colon - 1;
                 auto sub = broker.substr(colon + 1, len);
@@ -44,9 +40,9 @@ parse_brokers(std::vector<std::string>& brokers) noexcept {
                 port = (uint16_t)val;
             }
             host = broker.substr(0, colon);
-            addrs.push_back(net::unresolved_address{host, port});
+            addrs.emplace_back(host, port);
         }
-    } catch (const std::exception e) {
+    } catch (const std::exception& e) {
         ws_log.warn("failed to parse brokers: {}", e.what());
         return std::move(std::vector<net::unresolved_address>{});
     }
@@ -84,41 +80,43 @@ int main(int argc, char** argv) {
           brokers,
           c["brokers"].as<std::string>(),
           boost::algorithm::is_any_of(","));
-        if (brokers.size() == 0) {
+        if (brokers.empty()) {
             return ss::make_exception_future<int>(
               std::runtime_error("must specify at least one broker"));
         }
         auto addrs = parse_brokers(brokers);
-        if (addrs.size() == 0) {
+        if (addrs.empty()) {
             return ss::make_exception_future<int>(
               std::runtime_error("failed to parse brokers"));
         }
 
-        return ss::async([&]() {
+        return ss::async([&, addrs = std::move(addrs)]() {
             static kc::configuration cfg;
-            cfg.brokers.set_value(
-              std::vector<net::unresolved_address>({{"127.0.0.1", 9092}}));
+	    cfg.brokers.set_value(addrs);
             kc::client client{config::to_yaml(cfg, config::redact_secrets::no)};
 
-            client.connect().get();
-            auto res = client.is_connected().get();
-            ws_log.info("connected? {}", res);
-            if (res) {
-                kc::record_essence rec{};
-		rec.key = iobuf();
-                rec.key->append("key-abc", 7);
-		rec.value = iobuf();
-                rec.value->append("{ name: \"dave\" }", 16);
-                model::topic t{"hello"};
+            auto f = client.connect().then([&] {
+                return client.is_connected().then([&](bool res) {
+                    ws_log.info("connected? {}", res);
+                    if (res) {
+                        kc::record_essence rec{};
+                        rec.key = iobuf();
+                        rec.key->append("key-abc3", 7);
+                        rec.value = iobuf();
+                        rec.value->append("{ name: \"dave\" }", 16);
+                        const model::topic t{"hello-2"};
 
-                auto records = std::vector<kc::record_essence>();
-		records.push_back(std::move(rec));
+                        auto records = std::vector<kc::record_essence>();
+                        records.push_back(std::move(rec));
 
-		ws_log.info("here goes nothing...");
-                auto response
-                  = client.produce_records(t, std::move(records)).get();
-                ws_log.info("response: {}", response);
-            }
+                        ws_log.info("here goes nothing...");
+			return client.produce_records(t, std::move(records));
+                    }
+		    return ss::make_exception_future<kafka::produce_response>(std::runtime_error("poop"));
+                });
+            });
+	    auto response = f.get();
+	    ws_log.info("response: {}", response);
             return 0;
         });
     });
